3ef54b017978247b7d900715f39080081cb5f891
Arnak Melikyan <40056737+melikyan-a@users.noreply.github.com>
2021-07-10 19:08:45 +0400

    Update

--- api_project.js
+++ api_project.js
@@ -4,7 +4,7 @@ define({
   "description": "API документация",
   "title": "Проект Jsay",
   "url": "https://jsay-develop.herokuapp.com/",
-  "sampleUrl": false,
+  "sampleUrl": true,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
