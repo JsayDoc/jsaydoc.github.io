c881ce74dc95e209f52c98245e9c0cb9836f987a
Arnak <arnak@melikyan.am>
2021-07-10 19:42:43 +0400

    Added new api

--- api_data.js
+++ api_data.js
@@ -2,7 +2,7 @@ define({ "api": [
   {
     "type": "post",
     "url": "accounts/auth/",
-    "title": "Авторизация+",
+    "title": "Авторизация через почту",
     "success": {
       "examples": [
         {
@@ -18,7 +18,7 @@ define({ "api": [
             "type": "Object",
             "optional": false,
             "field": "token",
-            "description": "<p>Инфрмация о токене пользователя</p>"
+            "description": "<p>Информация о токене пользователя</p>"
           },
           {
             "group": "Response",
@@ -63,9 +63,9 @@ define({ "api": [
       }
     },
     "version": "1.0.0",
-    "name": "token",
+    "name": "auth",
     "group": "Аутентификация",
-    "description": "<p>Получение токена пользователя для последующих авторизованных запросов</p>",
+    "description": "<p>Авторизация через почту (форма логина)</p>",
     "parameter": {
       "fields": {
         "Parameter": [
@@ -82,6 +82,129 @@ define({ "api": [
             "optional": false,
             "field": "password",
             "description": "<p>Пароль пользователя, минимум <code>6</code> символов (минимум <code>1</code> цифра и <code>1</code> буква)</p>"
+          }
+        ]
+      }
+    },
+    "filename": "jsay/apps/account/rest/v1/api.py",
+    "groupTitle": "Аутентификация"
+  },
+  {
+    "type": "post",
+    "url": "accounts/registration/",
+    "title": "Регистрация через почту",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n    \"token\": {\n        \"refresh\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6M\",\n        \"access\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNT\"\n}\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "token",
+            "description": "<p>Информация о токене пользователя</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "token.refresh",
+            "description": "<p>Refresh токен для продления основного токена</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "token.access",
+            "description": "<p>Токен пользователя</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "{\n    \"error_code\": 1,\n    \"error_message\": \"Неверный емейл или пароль\"\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Error Code": [
+          {
+            "group": "Error Code",
+            "optional": false,
+            "field": "1",
+            "description": "<p>Неверный емейл или пароль</p>"
+          },
+          {
+            "group": "Error Code",
+            "optional": false,
+            "field": "2",
+            "description": "<p>Время блокировки в секундах</p>"
+          }
+        ]
+      }
+    },
+    "version": "1.0.0",
+    "name": "registration",
+    "group": "Аутентификация",
+    "description": "<p>Регистрация через почту (форма регистрации)</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Валидный e-mail - адрес почты</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Пароль пользователя, минимум <code>6</code> символов (минимум <code>1</code> цифра и <code>1</code> буква)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "first_name",
+            "description": "<p>Имя пользователя</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "water",
+            "description": "<p>Норма воды</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "allowedValues": [
+              "1",
+              "2"
+            ],
+            "optional": false,
+            "field": "gender",
+            "description": "<p>Пол пользователя, <code>1</code> - Муж., <code>2</code> - Жен.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "optional": true,
+            "field": "newsletters",
+            "defaultValue": "True",
+            "description": "<p>Подписка на новости</p>"
           }
         ]
       }
