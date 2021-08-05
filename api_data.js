fc0196b8b32c2f665870e7e5fbe588c859ec3c84
Arnak <arnak@melikyan.am>
2021-08-06 01:10:10 +0400

    Added new API's

--- api_data.js
+++ api_data.js
@@ -2039,7 +2039,7 @@ define({ "api": [
       "examples": [
         {
           "title": "1.Success-Response:",
-          "content": "{\n    \"check_email\": false,\n    \"check_full_info\": true,\n    \"daily_active\": {\n        \"notify\": true,\n        \"counter\": 0,\n        \"percent\": 100.0,\n        \"date\": \"2021-09-08\",\n        \"award\": {\n            \"title\": \"Вы пользуетесь приложением 1 дней\",\n            \"text\": \"Поздравляем и увидимся завтра!\",\n            \"photo_code\": \"da1\"\n        }\n    }\n}",
+          "content": "{\n    \"check_email\": false,\n    \"check_full_info\": false,\n    \"daily_active\": {\n        \"notify\": true,\n        \"counter\": 0,\n        \"percent\": 100.0,\n        \"date\": \"2021-09-08\",\n        \"award\": {\n            \"title\": \"Вы пользуетесь приложением 1 дней\",\n            \"text\": \"Поздравляем и увидимся завтра!\",\n            \"photo_code\": \"da1\"\n        }\n    }\n}",
           "type": "json"
         },
         {
