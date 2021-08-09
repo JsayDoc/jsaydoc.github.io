f321d6838056679d92134326c0f912b084553359
Arnak <arnak@melikyan.am>
2021-08-10 00:44:58 +0400

    Added new API's

--- api_data.js
+++ api_data.js
@@ -977,7 +977,7 @@ define({ "api": [
       "examples": [
         {
           "title": "Success-Response:",
-          "content": "{\n    \"drink_awards\": [\n        {\n            \"award\": {\n                \"title\": \"Почти аквариум!\",\n                \"text\": \"За последнее время вы выпили 40 л воды. Обычно такой объем у домашнего аквариума :) Продолжайте!\",\n                \"photo_code\": \"wd2\"\n            },\n            \"counter\": 0,\n            \"percent\": 62\n        },\n        {\n            \"award\": {\n                \"title\": \"Ваши первые 2 л!\",\n                \"text\": \"Продолжайте в том же духе и почувствуете себя лучше уже очень скоро.\",\n                \"photo_code\": \"wd1\"\n            },\n            \"counter\": 0,\n            \"percent\": 100\n        }\n    ],\n    \"active_awards\": [\n        {\n            \"award\": {\n                \"title\": \"Вы пользуетесь приложением 7 дней\",\n                \"text\": \"Вы пользуетесь приложением 7 дней. Отличный результат!\",\n                \"photo_code\": \"da2\"\n            },\n            \"counter\": 0\n        },\n        {\n            \"award\": {\n                \"title\": \"Вы пользуетесь приложением 1 дней\",\n                \"text\": \"Поздравляем и увидимся завтра!\",\n                \"photo_code\": \"da1\"\n            },\n            \"counter\": 0\n        }\n    ],\n    \"rate_awards\": [\n        {\n            \"award\": {\n                \"title\": \"Вы 1 день пьете свою норму воды\",\n                \"text\": \"Сегодня вы выпили свою нормы воды. Увидимся завтра!\",\n                \"photo_code\": \"dr1\"\n            },\n            \"counter\": 0\n        },\n        {\n            \"award\": {\n                \"title\": \"Вы 2 дней пьете свою норму воды\",\n                \"text\": \"Второй день с водой — вы отлично идете!\",\n                \"photo_code\": \"dr2\"\n            },\n            \"counter\": 0\n        }\n    ],\n    \"single_awards\": [\n        {\n            \"award\": {\n                \"text\": \"Первый пошёл: вы записали свой первый стакан воды. Продолжайте!\",\n                \"photo_code\": \"s1\"\n            }\n        }\n    ]\n}",
+          "content": "{\n    \"drink_awards\": [\n        {\n            \"counter\": 0,\n            \"percent\": 62,\n            \"award\": {\n                \"title\": \"Почти аквариум!\",\n                \"text\": \"За последнее время вы выпили 40 л воды. Обычно такой объем у домашнего аквариума :) Продолжайте!\",\n                \"photo_code\": \"wd2\"\n            }\n        },\n        {\n            \"counter\": 0,\n            \"percent\": 100,\n            \"award\": {\n                \"title\": \"Ваши первые 2 л!\",\n                \"text\": \"Продолжайте в том же духе и почувствуете себя лучше уже очень скоро.\",\n                \"photo_code\": \"wd1\"\n            }\n        }\n    ],\n    \"active_awards\": [\n        {\n            \"counter\": 0,\n            \"award\": {\n                \"title\": \"Вы пользуетесь приложением 7 дней\",\n                \"text\": \"Вы пользуетесь приложением 7 дней. Отличный результат!\",\n                \"photo_code\": \"da2\"\n            }\n        },\n        {\n            \"counter\": 0,\n            \"award\": {\n                \"title\": \"Вы пользуетесь приложением 1 дней\",\n                \"text\": \"Поздравляем и увидимся завтра!\",\n                \"photo_code\": \"da1\"\n            }\n        }\n    ],\n    \"rate_awards\": [\n        {\n            \"counter\": 0,\n            \"award\": {\n                \"title\": \"Вы 1 день пьете свою норму воды\",\n                \"text\": \"Сегодня вы выпили свою нормы воды. Увидимся завтра!\",\n                \"photo_code\": \"dr1\"\n            }\n        },\n        {\n            \"counter\": 0,\n            \"award\": {\n                \"title\": \"Вы 2 дней пьете свою норму воды\",\n                \"text\": \"Второй день с водой — вы отлично идете!\",\n                \"photo_code\": \"dr2\"\n            }\n        }\n    ],\n    \"single_awards\": [\n        {\n            \"award\": {\n                \"text\": \"Первый пошёл: вы записали свой первый стакан воды. Продолжайте!\",\n                \"photo_code\": \"s1\"\n            }\n        }\n    ]\n}",
           "type": "json"
         }
       ],
@@ -1072,6 +1072,76 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "active_awards.award.photo_code",
+            "description": "<p>Код миниатюры</p>"
+          },
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "rate_awards",
+            "description": "<p>Достижения дневной нормы</p>"
+          },
+          {
+            "group": "Response",
+            "type": "Number",
+            "optional": false,
+            "field": "rate_awards.counter",
+            "description": "<p>Счетчик повторений</p>"
+          },
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "rate_awards.award",
+            "description": "<p>Достижение</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "rate_awards.award.title",
+            "description": "<p>Название достижения</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "rate_awards.award.text",
+            "description": "<p>Текст достижения</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "rate_awards.award.photo_code",
+            "description": "<p>Код миниатюры</p>"
+          },
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "single_awards",
+            "description": "<p>Одиночные достижения</p>"
+          },
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "single_awards.award",
+            "description": "<p>Достижение</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "single_awards.award.text",
+            "description": "<p>Текст достижения</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "single_awards.award.photo_code",
             "description": "<p>Код миниатюры</p>"
           }
         ]
